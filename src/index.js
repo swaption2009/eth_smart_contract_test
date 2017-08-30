7207c513a8ed400524e633f918507fb07b41e5c2
swaption2009 <swaption2009@gmail.com>
2017-08-30 12:02:29 -0700

    web3 provider

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 let app = document.querySelector('#app')
 
+var Web3 = require('web3');
+var web3 = new Web3();
+window.web3 = web3;
+
+web3.setProvider(
+  new Web3.providers.HttpProvider('http://localhost:8545')
+  );
+
 app.innerHTML = '<h2>Welcome to hello-counter</h2>'
